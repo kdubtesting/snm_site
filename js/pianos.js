24c26d3f3d06c4e246fbed9a78b7a9582822d04a
kdubtesting <32721576+kdubtesting@users.noreply.github.com>
2018-06-03 17:50:43 -0700

    change

--- js/pianos.js
+++ js/pianos.js
@@ -50,6 +50,20 @@ for(var i = 0; i < openBar.length; i++)
 var steinSel = document.getElementById("stein-select");
 var steinName = document.querySelectorAll(".stein-name");
 var steinNameA = new Array();
+var steinImg = document.getElementById("stein-img");
+var steinImgs = ["img/Steinway/pianoA.jpg", "img/Steinway/pianoB.jpg", "img/Steinway/pianoD.jpg",
+"img/Steinway/pianoM.jpg", "img/Steinway/pianoO.jpg", "img/Steinway/pianoS.jpg"];
+var infoOnPara = document.querySelectorAll(".info-on-piano");
+var steinSubName = document.getElementById("stein-sub-name");
+var steinSub = ["salon grand", "classic grand", "concert grand", "studio grand", "living room grand", "city grand"];
+var steinPara = ["At 6' 2\" (188 cm), the Model A is known for delivering a “grand” sound in a medium-scale instrument. This grand offers power and warmth, with a design that allows the solid spruce soundboard to freely and efficiently resonate, like its larger counterparts.",
+"This magnificent 6' 11\" (211 cm) grand piano is often referred to by pianists as “the perfect piano.” It is a wonderfully balanced and versatile instrument that does extremely well in refined homes, teaching studios, and mid-sized venues and is also available as a STEINWAY SPIRIO, the world’s finest high resolution player piano.",
+"At 8' 11¾\" (274 cm) in length, this majestic musical instrument — the pinnacle of concert grands — is the overwhelming choice of the world's greatest pianists and for anyone who demands the highest level of musical expression.",
+"At 5' 7\" (170 cm) in length, this piano's rich tone, responsive action and manageable size makes it perfect for conservatories, and many homes as well. The Model M is also available as a STEINWAY SPIRIO, the world’s finest high resolution player piano",
+"At 5' 10¾\" (180 cm), the Model O is the largest of Steinway’s “small grands.” This piano is large enough to satisfy those who demand a full, rich sound, yet sized to fit in almost any home",
+"At 5' 1\" (155 cm), this piano is the smallest of the Steinway grands. This design was introduced in the 1930s to invite the majesty of the Steinway sound into almost any space."];
+var steinNameInfo = document.getElementById("stein-piano-name");
+var grandSec = document.querySelectorAll(".grand-sec");
 
 for(var i = 0; i < steinName.length; i++)
 {
@@ -58,7 +72,38 @@ for(var i = 0; i < steinName.length; i++)
 	{
 		var n = steinNameA.indexOf(this);
 		var content = steinName[n].innerHTML;
+		steinImg.setAttribute("src", steinImgs[n]);
 		steinSel.innerHTML = content;
+		steinNameInfo.innerHTML = content;
+		steinSubName.innerHTML = steinSub[n];
+		infoOnPara[0].innerHTML = steinPara[n];
+		if(n == 0)
+		{
+			steinImg.style.width = "60vw";
+		}
+		else if(n == 1)
+		{
+			steinImg.style.width = "65vw";
+		}
+		else if(n == 2)
+		{
+			steinImg.style.width = "80%";
+		}
+		else if(n == 3)
+		{
+			steinImg.style.width = "60vw";
+		}
+		else if(n == 4)
+		{
+			steinImg.style.width = "63%";
+		}
+		else if(n == 5)
+		{
+			steinImg.style.width = "60%";
+		}
+		selectBar[0].style.height = "2em";
+		openBar[0].style.display = "block";
+		closeBar[0].style.display = "none";
 	});
 }
 
