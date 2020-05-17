2b8fcee8861e0162057ec14a695d0d39b64665da
kdubtesting <32721576+kdubtesting@users.noreply.github.com>
2020-05-17 01:05:50 -0700

    moretest

--- scrolling.js
+++ scrolling.js
@@ -8,13 +8,91 @@ $(document).on("scroll", function(){
 	}
 });
 
+//menu
+var menu = document.getElementById("menu");
+var menuH = parseInt($(menu).css("height"));
 
-var titleDisplay = document.getElementById("title-display")
-var titleH = parseInt($(titleDisplay).css("height"));
-var titleT = parseInt($(titleDisplay).css("top"));
+//Company Header
+var companyName = document.getElementById("company-name");
+var companyNameH = parseInt($(companyName).css("height"));
+var companyNameW = parseInt($(companyName).css("width"));
 
-$("#more-stuff").css("top", ((2 * titleT) + titleH) + "px");
+var companyNameT = (menuH / 2) - (companyNameH / 2);
 
-var bodyH = (3 * titleT) + (2 * titleH);
+$(companyName).css("margin-top", companyNameT + "px");
 
-$(document.body).css("height", bodyH + "px");
+//years op
+var yearsOp = document.getElementById("years-op");
+var yearsOpH = parseInt($(yearsOp).css("height"));
+
+var yearsOpL = (1.5 * 16) + 10 + companyNameW;
+var yearsOpT = (menuH / 2) - (yearsOpH / 2);
+
+var totalTitleLength = (1.5 * 16) + companyNameW + 10 + parseInt($(yearsOp).css("width")) + 10;
+
+$(yearsOp).css("left", yearsOpL + "px");
+$(yearsOp).css("top", yearsOpT + "px");
+
+//This will be the link area
+var linkButtons = document.getElementsByClassName("link-button")
+var linkTexts = document.getElementsByClassName("link-text");
+
+var linksButtonL = 0;
+
+for(var i = 0; i < linkButtons.length; i++)
+{
+	var linkTextW = parseInt($(linkTexts[i]).css("width"));
+	var linkTextH = parseInt($(linkTexts[i]).css("height"));
+	var linkTextT = (menuH / 2) - (linkTextH / 2);
+	var linkButtonW = linkTextW + 40;
+
+	$(linkTexts[i]).css("margin-top", linkTextT + "px");
+	$(linkButtons[i]).css("left", linksButtonL + "px");
+	linksButtonL += linkButtonW;
+}
+
+$("#links-area").css("width", linksButtonL + "px");
+
+if((window.innerWidth - linksButtonL) < totalTitleLength)
+{
+	$("#links-area").css("display", "none");
+}
+
+for(var i = 0; i < linkButtons.length; i++)
+{
+	$(linkButtons[i]).on("mouseenter", function(){
+		$(this.childNodes[1]).css("border-bottom", "2px solid #000")
+	});
+
+	$(linkButtons[i]).on("mouseleave", function(){
+		$(this.childNodes[1]).css("border-bottom", "2px solid #fff")
+	});
+}
+
+$(window).on("resize", function(){
+	if((window.innerWidth - linksButtonL) < totalTitleLength)
+	{
+		$("#links-area").css("display", "none");
+	}
+});
+
+/*
+$(linkButtons[0]).on("mouseenter", function(){
+	$(linkTexts[0]).css("border-bottom", "2px solid #000");
+});
+
+$(linkButtons[0]).on("mouseleave", function(){
+	$(linkTexts[0]).css("border-bottom", "2px solid #fff");
+});
+
+
+$(linkButtons[1]).on("mouseenter", function(){
+	$(linkTexts[1]).css("border-bottom", "2px solid #000");
+});
+
+
+$(linkButtons[1]).on("mouseleave", function(){
+	$(linkTexts[1]).css("border-bottom", "2px solid #fff");
+});
+*/
+
