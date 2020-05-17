5762fc5942f774df7c8ee9101dafefbcf5c10af5
kdubtesting <32721576+kdubtesting@users.noreply.github.com>
2020-05-16 19:57:02 -0700

    more testing

--- scrolling.js
+++ scrolling.js
@@ -8,6 +8,13 @@ $(document).on("scroll", function(){
 	}
 });
 
-var isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
 
-console.log(isChrome);
+var titleDisplay = document.getElementById("title-display")
+var titleH = parseInt($(titleDisplay).css("height"));
+var titleT = parseInt($(titleDisplay).css("top"));
+
+$("#more-stuff").css("top", ((2 * titleT) + titleH) + "px");
+
+var bodyH = (3 * titleT) + (2 * titleH);
+
+$(document.body).css("height", bodyH + "px");
