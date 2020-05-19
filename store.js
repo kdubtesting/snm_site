525060ef6cdd9a7db540b71b02b6a043ea30cc78
kdubtesting <32721576+kdubtesting@users.noreply.github.com>
2020-05-19 00:55:41 -0700

    test

--- store.js
+++ store.js
@@ -1,4 +1,4 @@
-var windowW = Math.floor(0.9 * window.innerWidth);
+var windowRef = Math.floor(0.9 * window.innerWidth);
 
 var storeBody = document.getElementById("store-body");
 
@@ -7,83 +7,167 @@ var options = [["all products", "cigars", "cigarettes", "bongs", "hookahs"],
 
 var random = Math.floor(Math.random() * 4);
 
+//Divde function into three parts
+//The first part is what is the maximum width the cards can be,
+//and how they relate to the size of the screen
 
-function createProductCard(number, windW)
-{
-	var leftPos = 0;
-	var topPos = 0;
-	while(storeBody.childNodes.length != 0)
+var firstCard = document.createElement("DIV");
+firstCard.className = "product-card";
+storeBody.removeChild(storeBody.childNodes[0]);
+storeBody.appendChild(firstCard);
+
+//This is the image being added to the card
+var firstImage = document.createElement("DIV");
+firstImage.className = "product-image";
+
+storeBody.childNodes[0].appendChild(firstImage);
+
+//get the width of the card
+//This is the original width
+var fCardW = parseInt($(storeBody.childNodes[0]).css("width"));
+
+
+//This is the original height
+var fCardH = parseInt($(storeBody.childNodes[0]).css("height"));
+
+
+//This will be the ration between height and width
+var ratioHW = fCardH / fCardW;
+var ratioWH = fCardW / fCardH;
+var fiftyOGH = 0.5 * fCardH;
+
+//These variables will be for sizing purposes
+var maxWidth = (fCardW * 4) + 3;
+
+$(window).on("load", function(){
+	if((window.innerWidth - 16) <= maxWidth)
 	{
-		storeBody.removeChild(storeBody.childNodes[0]);
-	}
-	for(var i = 0; i < window.options[1][number]; i++)
-	{
-		var card = document.createElement("DIV");
-		card.className = "product-card";
+		var threeWidth = (3 * fCardH) + 2;
+		$(storeBody.childNodes[0]).css("width", fCardW + "px");
+		$(storeBody.childNodes[0]).css("height", fCardH + "px");
+		$(storeBody).css("height", fCardH + "px");
+		$(storeBody).css("width", threeWidth + "px");
+		$(storeBody).css("left", ((window.innerWidth / 2) - (threeWidth / 2)) + "px");
 
-		//Create Image block
-		var image = document.createElement("DIV");
-		image.className = "product-image";
-
-		card.appendChild(image);
-
-		//Create Info block
-		var info = document.createElement("DIV");
-
-		//append element
-		storeBody.appendChild(card);
-
-		//position elements
-		var cardW = parseInt($(storeBody.childNodes[0]).css("width"));
-		var ogLeft = leftPos;
-		leftPos += (cardW + 15)
-
-
-		if(leftPos <= windW)
+		if((window.innerWidth - 16) <= threeWidth)
 		{
-			$(storeBody.childNodes[i]).css("top", topPos + "px");
-			$(storeBody.childNodes[i]).css("left", ogLeft + "px");
+			var smallWidth = window.innerWidth - 16 - 3;
+			var setCardW = smallWidth / 3;
+			var setCardH = ratioHW * setCardW;
+			$(storeBody.childNodes[0]).css("height", setCardH + "px");
+			$(storeBody.childNodes[0]).css("width", setCardW + "px");
+			$(storeBody).css("width", (window.innerWidth - 16) + "px");
+			$(storeBody).css("height", setCardH + "px");
+			$(storeBody).css("left", (8) + "px");
+		}
+	}
+	else
+	{
+		$(storeBody).css("width", maxWidth + "px");
+		$(storeBody).css("height", fCardH + "px");
+		$(storeBody).css("left", ((window.innerWidth / 2) - (maxWidth / 2)) + "px");
+	}
+});
+
+$(window).on("resize", function(){
+	if((window.innerWidth - 16) <= maxWidth)
+	{
+		var threeWidth = (3 * fCardW) + 2;
+		$(storeBody.childNodes[0]).css("width", fCardW + "px");
+		$(storeBody.childNodes[0]).css("height", fCardH + "px");
+		$(storeBody).css("height", fCardH + "px");
+		$(storeBody).css("width", threeWidth + "px");
+		$(storeBody).css("left", ((window.innerWidth / 2) - (threeWidth / 2)) + "px");
+
+		if((window.innerWidth - 16) <= threeWidth)
+		{
+			var remainWidth = window.innerWidth - 16 - 3;
+			var newCardW = remainWidth / 3;
+			var newCardH = ratioHW * newCardW;
+			$(storeBody.childNodes[0]).css("height", newCardH + "px");
+			$(storeBody.childNodes[0]).css("width", newCardW + "px");
+			$(storeBody).css("width", (window.innerWidth - 16) + "px");
+			$(storeBody).css("height", newCardH + "px");
+			$(storeBody).css("left", (8) + "px");
+
+			if(newCardH <= fiftyOGH)
+			{
+				console.log(window.innerWidth - 16);
+				var remainSWidth = (window.innerWidth - 16) - 1;
+				var newCardSW = remainWidth * 0.5;
+				var newCardSH = newCardW + (7 * 16);
+				$(storeBody.childNodes[0]).css("height", newCardSH + "px");
+				$(storeBody.childNodes[0]).css("width", newCardSW + "px");
+				$(storeBody).css("width", (window.innerWidth - 16) + "px");
+				$(storeBody).css("height", newCardSH + "px");
+				$(storeBody).css("left", (8) + "px");
+			}
+		}
+	}
+	else
+	{
+		$(storeBody.childNodes[0]).css("width", fCardW + "px");
+		$(storeBody.childNodes[0]).css("height", fCardH + "px");
+		$(storeBody).css("width", maxWidth + "px");
+		$(storeBody).css("height", fCardH + "px");
+		$(storeBody).css("left", ((window.innerWidth / 2) - (maxWidth / 2)) + "px");
+	}
+});
+
+/*
+var testCard = document.createElement("DIV");
+testCard.className = "product-card";
+storeBody.removeChild(storeBody.childNodes[0]);
+storeBody.appendChild(testCard)
+var testCardW = parseInt($(storeBody.childNodes[0]).css("width"));
+var testCardH = parseInt($(storeBody.childNodes[0]).css("height"));
+var ratioHW = testCardH / testCardW;
+var halfOGH = 0.6 * testCardH;
+var maxBreakPoint = testCardW * 3; //This is where the site will either get smaller or larger
+
+$(window).on("resize", function(){
+	windowRef = Math.floor(0.9 * window.innerWidth);
+	if(windowRef <= maxBreakPoint)
+	{
+		var cardW = (windowRef * (0.33) - 6);
+		var cardH = ratioHW * cardW;
+		if(cardH <= halfOGH)
+		{
+			cardW = parseInt($(storeBody.childNodes[0]).css("width"));
+			cardH = ratioHW * cardW;
+			var storeBodyW = (2 * cardW);
+			$(storeBody).css("width", storeBodyW + "px");
+			$(storeBody).css("height", cardH + "px")
+			$(storeBody).css("left", ((window.innerWidth / 2) - (storeBodyW / 2)) + "px");	
+		
+			if(storeBodyW > window.innerWidth)
+			{
+				storeBodyW = 0.9 * window.innerWidth;
+				cardH = storeBodyW;
+				cardW = storeBodyW;
+				$(storeBody.childNodes[0]).css("width", cardW + 'px');
+				$(storeBody.childNodes[0]).css("height", cardH + "px");
+				$(storeBody).css("left", "5vw");
+				$(storeBody).css("width", storeBodyW + "px");
+				$(storeBody).css("height", storeBodyW + "px");
+			}
+
 		}
 		else
 		{
-			$(storeBody).css("width", (ogLeft - 15) + "px");
-			$(storeBody).css("left", ((window.innerWidth / 2) - ((ogLeft - 15) / 2)) + "px");
-			leftPos = 0;
-			topPos += (parseInt($(storeBody.childNodes[0]).css('height')) + 15);
-			$(storeBody.childNodes[i]).css("left", leftPos + "px");
-			$(storeBody.childNodes[i]).css("top", topPos + "px");
-			leftPos += (cardW + 15);
+			$(storeBody).css("width", "90vw");
+			$(storeBody).css("left", "5vw");
+			$(storeBody.childNodes[0]).css('width', cardW + "px");
+			$(storeBody.childNodes[0]).css('height', cardH + "px");
 		}
-	}
 
-	if(topPos == 0)
+	}
+	else
 	{
-		$(storeBody).css("width", (leftPos - 15) + "px");
-		$(storeBody).css("left", ((window.innerWidth / 2) - ((leftPos - 15) / 2)) + "px");
+		$(storeBody).css("left", "5vw");
+		$(storeBody.childNodes[0]).css('width', testCardW + "px");
+		$(storeBody.childNodes[0]).css('height', testCardH + "px");
 	}
+})
+*/
 
-	topPos += (parseInt($(storeBody.childNodes[0]).css('height')) + 15);
-	$(storeBody).css("height", topPos + "px");
-	$(document.body).css("height", ((7 * 16) + topPos + 30) + "px");
-}
-
-createProductCard(random, windowW);
-console.log(storeBody.childNodes);
-for(var i = 0; i < storeBody.childNodes.length; i++)
-{
-	console.log("hello");
-	$(storeBody.childNodes[i]).on("mouseenter", function(){
-		$(this).css("box-shadow", "0 0 5px rgba(0,0,0,0.5)");
-	});
-	$(storeBody.childNodes[i]).on("mouseleave", function(){
-		$(this).css("box-shadow", "0 0 5px rgba(0,0,0,0.3)");
-	});
-}
-
-var storeBodyW = parseInt($(storeBody).css("width"));
-
-$(window).on("resize", function(){
-	var resizeWindow = Math.floor((0.9 * (window.innerWidth)));
-	$(storeBody).css("left", ((window.innerWidth / 2) - (storeBodyW / 2)) + "px")
-	createProductCard(random, resizeWindow);
-});
